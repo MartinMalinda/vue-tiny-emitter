aa9ad5cf51f989f57e2add75ba88ffa7ceadcdf7
martinmalinda <malindacz@gmail.com>
2020-10-07 18:04:41 +0200

    make the tests pass

--- __tests__/index.ts
+++ __tests__/index.ts
@@ -1,7 +1,27 @@
-import { emitter } from '../src';
+import { defineComponent, createApp, h, nextTick } from 'vue';
+import { once, emit, emitter, useSubscribe } from '../src/';
 
 describe('emitter', () => {
   it('can be accessed', () => {
     expect(!!emitter).toBe(true);
+    expect(!!emit).toBe(true);
+    expect(!!once).toBe(true);
+  });
+});
+
+describe('useSubscribe', () => {
+  it('works in a component', async () => {
+    let mockCb = jest.fn();
+    createApp(defineComponent({
+      setup() {
+        useSubscribe('foo', mockCb);
+        return () => h('div');
+      }
+    })).mount(document.body);
+
+    emit('foo', 'bar');
+    await nextTick(() => {
+      expect(mockCb).toBeCalledWith('bar');
+    });
   });
 });
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
-import mitt from 'mitt';
+import { TinyEmitter } from 'tiny-emitter';
 import { onUnmounted } from 'vue';
 
-export const emitter = mitt();
+export const emitter = new TinyEmitter();
 export function useSubscribe(eventName: string, cb: () => void) {
   emitter.on(eventName, cb);
-  onUnmounted(() => emitter.off(eventName, cb));
+  onUnmounted(() => {
+    emitter.off(eventName, cb)
+  });
 }
 
-export const emit = emitter.emit;
+export const once = emitter.once.bind(emitter);
+export const emit = emitter.emit.bind(emitter);
